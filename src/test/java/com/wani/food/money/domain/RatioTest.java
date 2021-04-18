3b2a43611e8299bc2f057cc1731ecb5d9bc70108
oen142 <oen142@naver.com>
2021-04-18 20:21:18 +0900

    feat : Ratio 퍼센트를 테스트 한다.

--- src/test/java/com/wani/food/money/domain/RatioTest.java
+++ src/test/java/com/wani/food/money/domain/RatioTest.java
@@ -1,5 +1,19 @@
 package com.wani.food.money.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.wani.food.generic.money.domain.Money;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RatioTest {
 
+    @Test
+    public void 퍼센트() {
+
+        Ratio tenPercent = Ratio.valueOf(0.1);
+        Money thousandWon = Money.wons(1000);
+
+        assertThat(tenPercent.of(thousandWon)).isEqualTo(Money.wons(100));
+    }
 }
